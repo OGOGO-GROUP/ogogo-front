2fa466404748efde7b4bd56839526946feeb80be
Keneshbekov Ulugbek <ulugbek.keneshbekov@bk.ru>
2021-06-12 13:23:22 +0600

    refactoring yarn build

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(favicon(__dirname + '/build/favicon.ico'))
 
 app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, 'build')))
+app.use("/counselling/triage/", express.static(path.join(__dirname, '/build')))
 app.get('/ping', function (req, res) {
     return res.send('pong')
 })
