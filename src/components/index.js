e5a189dde11d1e3e2acc7ad0d7b86b3aede44c7f
Keneshbekov Ulugbek <ulugbek.keneshbekov@bk.ru>
2021-07-23 16:46:47 +0600

    update

--- src/components/index.js
+++ src/components/index.js
@@ -2,13 +2,13 @@ import { Header } from "./Header/Header"
 import { Topline } from "./Topline/Topline"
 import { Courses } from "./Courses/Courses"
 import { WhyUs } from "./WhyUs/WhyUs"
+import { Footer } from "./Footer/Footer"
+import { Banner } from "./Banner/Banner"
 import { Online } from "./Online/Online"
 import { Reviews } from "./Reviews/Reviews"
 import { News } from "./News/News"
 import { Animated } from "./Animated/Animated"
 import { Certificate } from "./Certificate/Certificate"
-import { Footer } from "./Footer/Footer"
-import { Banner } from "./Banner/Banner"
 
 export {
     Header, Topline, Courses,
